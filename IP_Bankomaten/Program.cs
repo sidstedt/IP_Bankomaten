f6ceb134efb5d45779eebb83241f1050b79a185c
sidstedt <daniel.sidstedt@gmail.com>
2024-09-23 13:10:03 +0200

    Created a new method to initialize users and account. Made a writeline to test out the arrays.

--- IP_Bankomaten/Program.cs
+++ IP_Bankomaten/Program.cs
@@ -2,9 +2,38 @@
 {
     internal class Program
     {
+        static string[][] users;
+        static string[][] accounts;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            InitializeUsersAndAccount();
+
+            Console.WriteLine("Välkommen till Daniels Bank AB");
+            Console.WriteLine($"{users[0][0]}");
+            Console.WriteLine($"{accounts[0][1]}: {accounts[0][2]}");
+            Console.ReadKey();
+        }
+        public static void InitializeUsersAndAccount()
+        {
+            users = [
+        [ "910101-0101" , "12345"],
+                [ "920202-0202" , "12345"],
+                [ "930303-0303" , "12345"],
+                [ "940404-0404" , "12345"],
+                [ "950505-0505" , "12345"]
+        ];
+            accounts = [
+                [users[0][0], "Kortkonto" , "1000"],
+                [users[0][0], "Sparkonto" , "500"],
+                [users[1][0], "Kortkonto" , "2000"],
+                [users[1][0], "Sparkonto" , "1000"],
+                [users[2][0], "Kortkonto" , "3000"],
+                [users[2][0], "Sparkonto" , "1500"],
+                [users[3][0], "Kortkonto" , "4000"],
+                [users[3][0], "Sparkonto" , "2000"],
+                [users[4][0], "Kortkonto" , "5000"],
+                [users[4][0], "Sparkonto" , "2500"],
+                ];
         }
     }
 }
